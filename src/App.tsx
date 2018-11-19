a31784185830611a7a159eec2ac2a12b6953ea11
Christian Savard <me@savardc.com>
2018-11-19 11:05:15 -0500

    Works better if you include files

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Ninja Math coming soon...
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
